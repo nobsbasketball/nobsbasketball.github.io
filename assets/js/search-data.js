7ba4cfc2043238c40d516d4d1b7171dc0463e71b
nobsbasketball <eliteracernt@gmail.com>
2025-01-12 03:29:21 +0000

    Deploying to gh-pages from @ nobsbasketball/nobsbasketball.github.io@604b50f29c3a62651197f95fd6dc84d070969687 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -44,6 +44,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
+        },{id: "nav-about",
+          title: "About",
+          description: "Hi, I am a current high schooler with JV basketball experience. When I first started high school, I had no clue how to workout and practice in order to improve my skills and strength. On YouTube, I saw many different basketball videos showcasing different skills to work on and get better, but many of them didn&#39;t even help, and were a waste of time. In addition, I didn&#39;t know how to effectively workout, especially with time constraints due to the amount of homework I faced during school. While there are many basketball workout videos and training programs out there, many of them are either a waste of time, or a waste of money. Many players including myself struggle with performing effective drills and structuring their workouts in order to maximize their improvement. As a result, I decided to create this website in order to help others who want to quickly improve and become a better player on the court. On this website, you will find basketball drills and workout structures that I previously used and benefitted the most from. You will also find information relating not only to basketball, but school as well, in order for you to learn to maintain the balance between school and basketball as a student athlete. For those of you putting in the work but feeling like you aren&#39;t improving, check out the motivation tab. In addition, I am also willing to create FREE personalized workout plans related to your specific situation. Every player is in a different situation. For instance, some are on the shorter side and struggle to get playing time on the court. Another player may have been the best on their JV team, but is struggling to get playing time on their Varsity team. To receive a FREE personalized workout plan made just for you, go to the workouts tab for more information.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/About/";
+          },
         },{id: "nav-people",
           title: "people",
           description: "members of the lab or group",
